ba9086c5af8f4a7d649f3e0a30e21cb63417741e
indexofmayank <maimayanmanu@gmail.com>
2024-08-04 12:37:02 +0530

    updateCategoryModelDone

--- src/components/UpdateCategoryModal.js
+++ src/components/UpdateCategoryModal.js
@@ -4,7 +4,6 @@ import {
   Input,
   FormControl,
   FormLabel,
-  FormHelperText,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -14,13 +13,9 @@ import {
   ModalCloseButton,
   useDisclosure,
   useToast,
-  Textarea,
   Center,
-  HStack,
-  Image,
-  VStack,
-  Checkbox,
   Text,
+  Image
 } from '@chakra-ui/react';
 import { useDropzone } from 'react-dropzone';
 import { useCategoryContext } from '../context/category_context';
@@ -35,17 +30,36 @@ const UpdateCategoryModal = ({ id }) => {
     fetchCategory,
     fetchSingleCategory
   } = useCategoryContext();
- 
+
+  const [imageList, setImageList] = useState(image);
+  const [loading, setLoading] = useState(false);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    if (acceptedFiles.length > 0) {
+      const file = acceptedFiles[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageList(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: 'image/jpeg, image/png',
+  });
+
   const { isOpen, onOpen, onClose } = useDisclosure();
   const initialRef = useRef();
   const toast = useToast();
+
   return (
     <>
       <Text
         colorScheme='brown'
         minW='100%'
         onClick={() => {
-          fetchSingleCategory(id);
           onOpen();
         }}
       >
@@ -58,6 +72,7 @@ const UpdateCategoryModal = ({ id }) => {
           <ModalHeader>Update category</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
+
             <FormControl>
               <FormLabel>Name</FormLabel>
               <Input
@@ -66,18 +81,48 @@ const UpdateCategoryModal = ({ id }) => {
                 name='name'
                 focusBorderColor='brown.500'
                 value={name}
-                onChange={ () => {}}
+                onChange={() => { }}
               />
             </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Images</FormLabel>
+              <Center
+                bg='brown.50'
+                minHeight={100}
+                my={5}
+                borderWidth={3}
+                borderColor='brown.200'
+                borderStyle='dashed'
+                borderRadius='lg'
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                {isDragActive ? (
+                  <p>Drag your files here</p>
+                ) : (
+                  <p>
+                    Drag drop image files here, or click to select files
+                    <br />
+                    (Only *.jpeg and *.png images will be accepted)
+                  </p>
+                )}
+              </Center>
+              {imageList && (
+                <Center>
+                  <Image src={imageList} alt="Category" maxH="200px" my={3} />
+                </Center>
+              )}
+            </FormControl>
+
           </ModalBody>
 
           <ModalFooter>
-          <Button mr={3} onClick={onClose}>
+            <Button mr={3} onClick={onClose}>
               Cancel
             </Button>
             <Button
               colorScheme='brown'
-              onClick={() => {}}
+              onClick={() => { }}
             >
               Save
             </Button>
@@ -86,7 +131,7 @@ const UpdateCategoryModal = ({ id }) => {
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
 
 export default UpdateCategoryModal;
